7889c52db22000743f759944f6a066d74f15c452
Samuel Yu <samuelyuyy@gmail.com>
2021-10-29 21:35:52 +0800

    Initial hw1

---
+++ hw1 KNN Matting/src/release.py
@@ -0,0 +1,90 @@
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+import scipy.sparse
+import warnings
+import cv2
+
+K = 10
+
+IMG_DIR = '../img'
+OUT_DIR = '../result'
+
+
+def knn_matting(img, trimap, my_lambda=100):
+    [h, w, c] = img.shape
+    img, trimap = img / 255.0, trimap / 255.0
+    foreground = (trimap == 1.0).astype(int)
+    background = (trimap == 0.0).astype(int)
+    all_constraints = foreground + background
+    img_size = h * w
+
+    # Calculate feature vector X
+    # Hint: X(i) = (R, G, B, x, y)(i)
+    rgb = img.reshape(img_size, c)
+    x, y = np.unravel_index(np.arange(img_size), (h, w))
+    xy = np.array([x, y]).T / np.sqrt(h * h + w * w)
+    X = np.append(rgb, xy, axis=1)
+
+    # Calculate nearest neighbors
+    nn = NearestNeighbors(n_neighbors=K, n_jobs=4).fit(X)
+    knn = nn.kneighbors(X)[1]
+
+    # Calculate kernel function k
+    # Hint: k(i, j) = 1 - |X(i) - X(j)| / C
+    i = np.repeat(np.arange(img_size), K)
+    j = knn.reshape(img_size * K)
+    C = c + 2
+    k = 1 - np.linalg.norm(X[i] - X[j], axis=1) / C
+
+    # Calculate the affinity matrix A
+    A = scipy.sparse.coo_matrix((k, (i, j)), shape=(img_size, img_size))
+
+    # Prepare for objective function
+    # Hint: obj = (L + lambda * M) * alpha - lambda * v
+    D = scipy.sparse.diags(np.ravel(A.sum(axis=1)))
+    L = D - A
+    M = scipy.sparse.diags(np.ravel(all_constraints[:, :, 0]))
+    v = np.ravel(foreground[:, :, 0]).T
+
+    # Solve for the linear system
+    warnings.filterwarnings('error')
+    alpha = []
+    try:
+        alpha = np.minimum(np.maximum(
+            scipy.sparse.linalg.spsolve(
+                L + my_lambda * M,
+                my_lambda * v,
+            ), 0), 1).reshape(h, w)
+    except Warning:
+        tmp = scipy.sparse.linalg.lsqr(
+            L + my_lambda * M,
+            my_lambda * v,
+        )
+        alpha = np.minimum(np.maximum(tmp[0], 0), 1).reshape(h, w)
+    return alpha
+
+
+def main():
+    img_names = ['bear.png', 'gandalf.png', 'woman.png']
+
+    for img_name in img_names:
+        print(f'Processing image {img_name}.')
+        image = cv2.imread(f'{IMG_DIR}/image/{img_name}')
+        trimap = cv2.imread(f'{IMG_DIR}/trimap/{img_name}')
+
+        alpha = knn_matting(image, trimap)
+        plt.title('Alpha Matte')
+        plt.imshow(alpha, cmap='gray')
+        plt.show()
+        alpha = alpha[:, :, np.newaxis]
+
+        
+
+        # cv2.imwrite('{OUT_DIR}/{img_name}', alpha * 255)
+
+        print('Done.')
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    main()
